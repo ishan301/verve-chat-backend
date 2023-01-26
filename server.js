30d18565d4e782ab30e66e0a5f5ff1bb836ad045
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-01-26 09:41:07 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+app.set('view-engine', 'ejs')
+app.set('views', __dirname+'/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT || 3000)

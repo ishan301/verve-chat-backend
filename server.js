2d3995b4862ecc63ea2f2eaa3f7d9b4763075db3
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-01-29 12:10:19 +0530

    connected with database vervechat in mongodb

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
-app.set('view-engine', 'ejs')
-app.set('views', __dirname+'/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const router = require("./routes/index");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT || 3000)
+app.set("view-engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once('open', () => console.log("connected to MongoDB"))
+
+app.use("/", router);
+app.listen(process.env.PORT || 3000);

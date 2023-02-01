82697710b2421f285dcba0b07c59e28b139d25f5
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-02-01 14:04:50 +0530

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const router = require("./routes/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.set("view-engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once('open', () => console.log("connected to MongoDB"))
+const PORT = process.env.PORT || 5000;
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));
+
 
 app.use("/", router);
-app.listen(process.env.PORT || 3000);
